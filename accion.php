0c98fca9a063a7287a54cbb29c3550b73ce3f1fb
ramon <monchouam@gmail.com>
2021-07-17 16:31:51 -0500

    creating a function to convert data to json and add it to a file .txt

--- accion.php
+++ accion.php
@@ -3,40 +3,27 @@
 session_cache_limiter('nocache');
 session_start();
 
+function addToFile($archivo, $datum){ 
+    $myJSON = json_encode($datum);
+    $file = fopen($archivo, 'a+');
+    fwrite($file, $myJSON."\n");
+    fclose($file); 
+}
+
+
 $name= $_GET['nombre'];
 $age= $_GET['edad'];
 
-//echo htmlspecialchars($name); 
-//echo (int)$age; 
-
-//$myArr = array();
-//$myArr[] = array("name"=>$name, "age"=>$age);
+$fichero = 'data1.txt';
 
 $myArr= array("name"=>$name, "age"=>$age);
+addToFile($fichero, $myArr);
 
-$myJSON = json_encode($myArr);
-//echo $myJSON;
 
-$file = fopen('data1.txt', 'a+');
-fwrite($file, $myJSON."\n");
-fclose($file); 
-/*
-$fichero = 'data1.txt';
-$cadena = implode(",",$myArr);
 
-// Abre el fichero para obtener el contenido existente
-$actual = file_get_contents($fichero);
-// Añade una nueva persona al fichero
-$actual .= $myArr;
-// Escribe el contenido al fichero
-file_put_contents($fichero, $actual);
-*/
-
-$fichero = 'data1.txt';
 $d= array();
 $datos = file($fichero);
 foreach($datos as $dato){
-    //$line= array($dato);
     $d[]= json_decode($dato);
 }
 echo json_encode($d);
